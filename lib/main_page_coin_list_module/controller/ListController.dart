0c0eb1827279eff35dd101f9b77f8dbc252feff5
ggams <union93@konkuk.ac.kr>
2021-07-25 21:12:31 +0900

    -animation background

--- lib/main_page_coin_list_module/controller/ListController.dart
+++ lib/main_page_coin_list_module/controller/ListController.dart
@@ -1,31 +1,3 @@
 import 'dart:convert';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
-import 'package:dyor_front/main_page_coin_list_module/model/CoinModel.dart';
-
-class ListController extends GetxController{
-
-  var coinList = List<CoinModel>().obs;
-  void onInit() {
-    fetchCoinList();
-    super.onInit();
-  }
-  Future<CoinModel> fetchCoinList() async {
-    final response =
-      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
-    if (response.statusCode ==200 ){
-      return CoinModel.fromJson(jsonDecode(response.body));
-    }else{
-      throw Exception('코인정보 로딩에 실패했습니다.');
-    }
-  }
-}
-
-class Listgenerator extends GetxController{
-
-
-
-
-}
-
-
---
+++ lib/main_page_coin_list_module/model/volume_desc.dart
@@ -0,0 +1,94 @@
+/// mainpage_tokens : [{"current_price":1.0,"market_cap":62048575854.0,"market_cap_change_percentage_24h":0.03827,"name":"Tether","price_change_percentage_24h":0.09819,"symbol":"usdt","total_volume":48697416682},{"current_price":34552.0,"market_cap":648846654459.0,"market_cap_change_percentage_24h":2.11935,"name":"Bitcoin","price_change_percentage_24h":2.06284,"symbol":"btc","total_volume":20983497034},{"current_price":2177.4,"market_cap":254391935750.0,"market_cap_change_percentage_24h":1.68351,"name":"Ethereum","price_change_percentage_24h":1.66442,"symbol":"eth","total_volume":18977511357},{"current_price":42.39,"market_cap":2332602229.0,"market_cap_change_percentage_24h":-7.57499,"name":"Axie Infinity","price_change_percentage_24h":-9.10829,"symbol":"axs","total_volume":4616702487},{"current_price":1.0,"market_cap":11736766641.0,"market_cap_change_percentage_24h":0.79635,"name":"Binance USD","price_change_percentage_24h":0.04086,"symbol":"busd","total_volume":4040401289},{"current_price":48.45,"market_cap":6238225756.0,"market_cap_change_percentage_24h":6.29624,"name":"Ethereum Classic","price_change_percentage_24h":6.36781,"symbol":"etc","total_volume":3610821424},{"current_price":453.69,"market_cap":8535888895.0,"market_cap_change_percentage_24h":-0.66178,"name":"Bitcoin Cash","price_change_percentage_24h":-0.75867,"symbol":"bch","total_volume":2956865114},{"current_price":0.03326761,"market_cap":16633804.0,"market_cap_change_percentage_24h":2.26685,"name":"ZBG Token","price_change_percentage_24h":2.24943,"symbol":"zt","total_volume":2885944840},{"current_price":0.152454,"market_cap":0.0,"market_cap_change_percentage_24h":0.0,"name":"DKK Token","price_change_percentage_24h":-0.66063,"symbol":"dkkt","total_volume":2067287121},{"current_price":0.200024,"market_cap":26118791978.0,"market_cap_change_percentage_24h":3.4949,"name":"Dogecoin","price_change_percentage_24h":3.58414,"symbol":"doge","total_volume":1807992553},{"current_price":1.0,"market_cap":27104346444.0,"market_cap_change_percentage_24h":0.2674,"name":"USD Coin","price_change_percentage_24h":0.02599,"symbol":"usdc","total_volume":1754296101},{"current_price":0.606851,"market_cap":28082040332.0,"market_cap_change_percentage_24h":-0.24631,"name":"XRP","price_change_percentage_24h":-0.28646,"symbol":"xrp","total_volume":1519867210},{"current_price":0.00255351,"market_cap":0.0,"market_cap_change_percentage_24h":0.0,"name":"ABC Chain","price_change_percentage_24h":-0.01141,"symbol":"abc","total_volume":1507461954},{"current_price":302.17,"market_cap":46720434838.0,"market_cap_change_percentage_24h":-0.26389,"name":"Binance Coin","price_change_percentage_24h":-0.27136,"symbol":"bnb","total_volume":1359914088},{"current_price":126.9,"market_cap":8471131330.0,"market_cap_change_percentage_24h":0.56752,"name":"Litecoin","price_change_percentage_24h":0.59189,"symbol":"ltc","total_volume":1299140659},{"current_price":0.7563,"market_cap":532178622.0,"market_cap_change_percentage_24h":2.66014,"name":"The Sandbox","price_change_percentage_24h":2.06008,"symbol":"sand","total_volume":1167197452},{"current_price":32.24,"market_cap":211473993.0,"market_cap_change_percentage_24h":37.06476,"name":"Augur","price_change_percentage_24h":37.34768,"symbol":"rep","total_volume":1150100595},{"current_price":1.23,"market_cap":39522608713.0,"market_cap_change_percentage_24h":1.96686,"name":"Cardano","price_change_percentage_24h":1.88609,"symbol":"ada","total_volume":1039779461},{"current_price":3.67,"market_cap":3524507371.0,"market_cap_change_percentage_24h":-0.02809,"name":"EOS","price_change_percentage_24h":0.00108,"symbol":"eos","total_volume":895467454},{"current_price":1.57,"market_cap":289985521.0,"market_cap_change_percentage_24h":-19.96637,"name":"Coin98","price_change_percentage_24h":-19.93453,"symbol":"c98","total_volume":833383321},{"current_price":9.23,"market_cap":1550057776.0,"market_cap_change_percentage_24h":1.98087,"name":"Huobi Token","price_change_percentage_24h":2.01212,"symbol":"ht","total_volume":789473492},{"current_price":43.99,"market_cap":6018076114.0,"market_cap_change_percentage_24h":26.69421,"name":"Internet Computer","price_change_percentage_24h":27.1401,"symbol":"icp","total_volume":768079788},{"current_price":12.47,"market_cap":216910594.0,"market_cap_change_percentage_24h":63.52386,"name":"My Neighbor Alice","price_change_percentage_24h":63.40364,"symbol":"alice","total_volume":759417789},{"current_price":0.057473,"market_cap":4119989023.0,"market_cap_change_percentage_24h":1.63769,"name":"TRON","price_change_percentage_24h":1.67324,"symbol":"trx","total_volume":757170664},{"current_price":0.948399,"market_cap":6067726494.0,"market_cap_change_percentage_24h":2.36194,"name":"Polygon","price_change_percentage_24h":2.55965,"symbol":"matic","total_volume":703356722},{"current_price":13.64,"market_cap":13820566073.0,"market_cap_change_percentage_24h":0.44718,"name":"Polkadot","price_change_percentage_24h":0.42842,"symbol":"dot","total_volume":683213887},{"current_price":0.281164,"market_cap":348776311.0,"market_cap_change_percentage_24h":10.4364,"name":"Alien Worlds","price_change_percentage_24h":8.02482,"symbol":"tlm","total_volume":672840946},{"current_price":6.08,"market_cap":6074552919.0,"market_cap_change_percentage_24h":8.49785,"name":"Theta Network","price_change_percentage_24h":9.48767,"symbol":"theta","total_volume":669766472},{"current_price":98.85,"market_cap":1120354906.0,"market_cap_change_percentage_24h":-0.02322,"name":"Zcash","price_change_percentage_24h":-0.01418,"symbol":"zec","total_volume":653926274},{"current_price":16.86,"market_cap":7442608011.0,"market_cap_change_percentage_24h":-0.64278,"name":"Chainlink","price_change_percentage_24h":-0.3057,"symbol":"link","total_volume":622953572},{"current_price":49.62,"market_cap":4500548479.0,"market_cap_change_percentage_24h":5.89839,"name":"Filecoin","price_change_percentage_24h":6.01001,"symbol":"fil","total_volume":552861883},{"current_price":0.072097,"market_cap":4704462272.0,"market_cap_change_percentage_24h":0.62332,"name":"VeChain","price_change_percentage_24h":0.58118,"symbol":"vet","total_volume":483850411},{"current_price":141.9,"market_cap":1454747288.0,"market_cap_change_percentage_24h":-2.51738,"name":"Dash","price_change_percentage_24h":-2.47368,"symbol":"dash","total_volume":459917996},{"current_price":302.05,"market_cap":0.0,"market_cap_change_percentage_24h":0.0,"name":"Wrapped BNB","price_change_percentage_24h":-0.21695,"symbol":"wbnb","total_volume":449655978},{"current_price":11.44,"market_cap":3165111746.0,"market_cap_change_percentage_24h":1.27647,"name":"Cosmos","price_change_percentage_24h":1.20024,"symbol":"atom","total_volume":424295404},{"current_price":1.0,"market_cap":5307477855.0,"market_cap_change_percentage_24h":1.40747,"name":"Dai","price_change_percentage_24h":0.02014,"symbol":"dai","total_volume":404010264},{"current_price":0.410448,"market_cap":230381741.0,"market_cap_change_percentage_24h":15.61819,"name":"Chromia","price_change_percentage_24h":15.61017,"symbol":"chr","total_volume":402930329},{"current_price":0.263313,"market_cap":6151078181.0,"market_cap_change_percentage_24h":-0.85525,"name":"Stellar","price_change_percentage_24h":-0.83755,"symbol":"xlm","total_volume":389282207},{"current_price":19.5,"market_cap":64991267.0,"market_cap_change_percentage_24h":28.60645,"name":"DeXe","price_change_percentage_24h":26.39735,"symbol":"dexe","total_volume":380919090},{"current_price":3.82,"market_cap":0.0,"market_cap_change_percentage_24h":0.0,"name":"Dawn Protocol","price_change_percentage_24h":0.11641,"symbol":"dawn","total_volume":379193940},{"current_price":14.22,"market_cap":3804992977.0,"market_cap_change_percentage_24h":0.49993,"name":"OKB","price_change_percentage_24h":-0.2119,"symbol":"okb","total_volume":373818495},{"current_price":8.24,"market_cap":3443287906.0,"market_cap_change_percentage_24h":4.35798,"name":"Terra","price_change_percentage_24h":4.60851,"symbol":"luna","total_volume":360436691},{"current_price":19.88,"market_cap":1060209425.0,"market_cap_change_percentage_24h":-6.17013,"name":"Flow","price_change_percentage_24h":-5.05608,"symbol":"flow","total_volume":321871532},{"current_price":28.3,"market_cap":7718885813.0,"market_cap_change_percentage_24h":-2.44207,"name":"Solana","price_change_percentage_24h":-2.23551,"symbol":"sol","total_volume":303479338},{"current_price":18.23,"market_cap":9477751809.0,"market_cap_change_percentage_24h":-2.49926,"name":"Uniswap","price_change_percentage_24h":-2.3163,"symbol":"uni","total_volume":303068565},{"current_price":3.78,"market_cap":52412068.0,"market_cap_change_percentage_24h":34.60333,"name":"Alpha Quark Token","price_change_percentage_24h":35.22727,"symbol":"aqt","total_volume":296867997},{"current_price":0.714329,"market_cap":946922373.0,"market_cap_change_percentage_24h":0.97584,"name":"Decentraland","price_change_percentage_24h":1.14315,"symbol":"mana","total_volume":291354843},{"current_price":0.249928,"market_cap":1335333673.0,"market_cap_change_percentage_24h":-3.4161,"name":"Chiliz","price_change_percentage_24h":-2.75868,"symbol":"chz","total_volume":288249113},{"current_price":8.2,"market_cap":1582245395.0,"market_cap_change_percentage_24h":-1.81059,"name":"Sushi","price_change_percentage_24h":-1.71076,"symbol":"sushi","total_volume":282319955},{"current_price":6.41e-06,"market_cap":3192081318.0,"market_cap_change_percentage_24h":-0.6912,"name":"Shiba Inu","price_change_percentage_24h":-0.79025,"symbol":"shib","total_volume":275858778}]
+
+class Volume_desc {
+  List<Mainpage_tokens> _mainpageTokens;
+  List<Mainpage_tokens> get mainpageTokens => _mainpageTokens;
+
+  Volume_desc({
+      List<Mainpage_tokens> mainpageTokens}){
+    _mainpageTokens = mainpageTokens;
+}
+
+  Volume_desc.fromJson(dynamic json) {
+    if (json["mainpage_tokens"] != null) {
+      _mainpageTokens = [];
+      json["mainpage_tokens"].forEach((v) {
+        _mainpageTokens.add(Mainpage_tokens.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    if (_mainpageTokens != null) {
+      map["mainpage_tokens"] = _mainpageTokens.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+}
+
+/// current_price : 1.0
+/// market_cap : 62048575854.0
+/// market_cap_change_percentage_24h : 0.03827
+/// name : "Tether"
+/// price_change_percentage_24h : 0.09819
+/// symbol : "usdt"
+/// total_volume : 48697416682
+
+class Mainpage_tokens {
+  double _currentPrice;
+  double _marketCap;
+  double _marketCapChangePercentage24h;
+  String _name;
+  double _priceChangePercentage24h;
+  String _symbol;
+  int _totalVolume;
+
+  double get currentPrice => _currentPrice;
+  double get marketCap => _marketCap;
+  double get marketCapChangePercentage24h => _marketCapChangePercentage24h;
+  String get name => _name;
+  double get priceChangePercentage24h => _priceChangePercentage24h;
+  String get symbol => _symbol;
+  int get totalVolume => _totalVolume;
+
+  Mainpage_tokens({
+      double currentPrice, 
+      double marketCap, 
+      double marketCapChangePercentage24h, 
+      String name, 
+      double priceChangePercentage24h, 
+      String symbol, 
+      int totalVolume}){
+    _currentPrice = currentPrice;
+    _marketCap = marketCap;
+    _marketCapChangePercentage24h = marketCapChangePercentage24h;
+    _name = name;
+    _priceChangePercentage24h = priceChangePercentage24h;
+    _symbol = symbol;
+    _totalVolume = totalVolume;
+}
+
+  Mainpage_tokens.fromJson(dynamic json) {
+    _currentPrice = json["current_price"];
+    _marketCap = json["market_cap"];
+    _marketCapChangePercentage24h = json["market_cap_change_percentage_24h"];
+    _name = json["name"];
+    _priceChangePercentage24h = json["price_change_percentage_24h"];
+    _symbol = json["symbol"];
+    _totalVolume = json["total_volume"];
+  }
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    map["current_price"] = _currentPrice;
+    map["market_cap"] = _marketCap;
+    map["market_cap_change_percentage_24h"] = _marketCapChangePercentage24h;
+    map["name"] = _name;
+    map["price_change_percentage_24h"] = _priceChangePercentage24h;
+    map["symbol"] = _symbol;
+    map["total_volume"] = _totalVolume;
+    return map;
+  }
+
+}
